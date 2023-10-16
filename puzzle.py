ee606a66d4cfd6ee439e3ebe2bbef55c137b549a
Lisandra Melo <mendie73@gmail.com>
2023-10-16 14:56:31 -0300

    Complete Project

---
+++ puzzle.py
@@ -0,0 +1,106 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    # Ground Rules: A cannot be both, but it must one
+    Or(AKnight, AKnave), Not(And(AKnave, AKnight)),
+    # Knowledge from the sentence
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnight, Not(And(AKnight, AKnave)))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    # Ground Rules: A cannot be both, but it must one
+    Or(AKnight, AKnave), Not(And(AKnave, AKnight)),
+    # Ground Rules: B cannot be both, but it must one
+    Or(BKnight, BKnave), Not(And(BKnave, BKnight)),
+    # Knowledge from the sentence
+    Implication(AKnight, And(AKnave,BKnave)),
+    Implication(AKnave, Not(And(AKnave,BKnave)))
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    # Ground Rules: A cannot be both, but it must one
+    Or(AKnight, AKnave), Not(And(AKnave, AKnight)),
+    # Ground Rules: B cannot be both, but it must one
+    Or(BKnight, BKnave), Not(And(BKnave, BKnight)),
+    # Knowledge from the sentence 1
+    Implication(AKnight, Or(And(AKnave,BKnave), And(AKnight,BKnight))),
+    Implication(AKnave, Not(Or(And(AKnave,BKnave), And(AKnight,BKnight)))),
+    # Knowledge from the sentence 2
+    Implication(BKnight, Or(And(AKnave,BKnight), And(AKnight,BKnave))),
+    Implication(BKnave, Not(Or(And(AKnave,BKnight), And(AKnight,BKnave)))),
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    # Ground Rules: A cannot be both, but it must one
+    Or(AKnight, AKnave), 
+    Not(And(AKnave, AKnight)),
+    # Ground Rules: B cannot be both, but it must one
+    Or(BKnight, BKnave),
+    Not(And(BKnave, BKnight)),
+    # Ground Rules: C cannot be both, but it must one
+    Or(CKnight, CKnave),
+    Not(And(CKnave, CKnight)),
+    # Knowledge from sentence 1
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnight, AKnave))),
+    # Knowledge from sentence 2
+    Implication(BKnight, And(
+        Implication(AKnight, AKnave),Implication(AKnave,Not(AKnave)))
+        ),
+    Implication(BKnave, Not(And(
+        Implication(AKnight, AKnave),Implication(AKnave,Not(AKnave)))
+        )),
+    # Knowledge from sentence 3
+    Implication(BKnight,CKnave),
+    Implication(BKnave,Not(CKnave)),
+    # Knowledge from sentence 4
+    Implication(CKnight,AKnight),
+    Implication(CKnave,Not(AKnight))
+)
+    
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
